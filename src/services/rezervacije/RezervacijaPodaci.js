85f624f5a062c16744059b59d24e735c8a4317f0
snalcermaja <snalcermaja2@gmail.com>
2026-05-15 10:37:57 +0200

    Update RezervacijaPodaci.js

--- src/services/rezervacije/RezervacijaPodaci.js
+++ src/services/rezervacije/RezervacijaPodaci.js
@@ -1,9 +1,10 @@
 export const rezervacije = [
     {
-        sifra: 1,
-        korisnik: 1,
-        datum: '2026-04-28T10:00:00',
-        napomena: 'Nema',
-        usluge: [1,2,3]
+        cijena: 240,
+        datumKraja: '2026-04-04T1',
+        datumPočetka: '2026-04-02',
+        gost: 1,
+        platio: true,
+        sifra: 1
     }
 ]
